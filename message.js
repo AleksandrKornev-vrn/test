c04ea3aad55bc8d9460b02bb963a8bbd5f92ab1c
Александр Корнев <koresh631@mail.ru>
2024-01-01 17:55:41 +0300

    Пробуем заливать ветку на гитхаб, Верисия 1

--- message.js
+++ message.js
@@ -6,6 +6,6 @@ var containerText = document.querySelector(".container-text")
 
 containerText.addEventListener ("click", function () {displayMessage ("Не спи!");});
 
-containerText.addEventListener ("click", function () {displayMessage ("Ну что ж успехи на лицо");});
+containerText.addEventListener ("click", function () {displayMessage ("Ну что ж успехи на лицо и яйцо");});
 
 containerText.addEventListener ("click", function () {displayMessage ("Я проснулся и сейчас пойду за шампанским!");})
