bfeb259dedb55a1462cfd409ba1b5edfefc3bb9f
Александр Корнев <koresh631@mail.ru>
2024-01-01 16:54:44 +0300

    добаротал выводимые сообщения

--- message.js
+++ message.js
@@ -6,4 +6,4 @@ var containerText = document.querySelector(".container-text")
 
 containerText.addEventListener ("click", function () {displayMessage ("Не спи!");});
 
-containerText.addEventListener ("click", function () {displayMessage ("Я проснулся!");})
+containerText.addEventListener ("click", function () {displayMessage ("Я проснулся и сейчас пойду за шампанским!");})
